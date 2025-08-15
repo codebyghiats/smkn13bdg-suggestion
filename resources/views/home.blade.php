44e8aa97e9f7dd5c9986fcf7d116d2aa4ab9a19e
Ghiats Abdurahman Rasyid <ghiatsajalah@gmail.com>
2025-08-16 00:14:20 +0700

    navbar

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,8 +4,32 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Home</title>
+    <link rel="stylesheet" href="css/style.css">
+    <link
+    rel="stylesheet"
+    type="text/css"
+    href="https://cdn.jsdelivr.net/npm/@phosphor-icons/web@2.1.1/src/regular/style.css"
+    />
+    <link
+    rel="stylesheet"
+    type="text/css"
+    href="https://cdn.jsdelivr.net/npm/@phosphor-icons/web@2.1.1/src/fill/style.css"
+    />
 </head>
 <body>
+    <nav class="navbar">
+        <div class="navbar-container">
+            <div class="navbar-logo">
+                <!-- Tempat logo -->
+                <img src="img/Analis_Bdg.jpg" alt="Logo" style="height:64px;" />
+            </div>
+            <div class="navbar-menu">
+                <a href="/">Home</a>
+                <a href="/about">About</a>
+                <a href="/contact">Contact</a>
+            </div>
+        </div>
+    </nav>
     <h2>Hello World!</h2>
 </body>
 </html>
